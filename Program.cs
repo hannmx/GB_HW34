5a2503cab8006a12cad3f749289e4de982969da7
hannmx <mxkud@mail.ru>
2022-11-28 22:17:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_HW34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rand = new Random();
+            
+            void PrintArray(int[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+
+            void FillArray(int[] arr)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                   arr[i] = rand.Next(100, 1000);
+                }
+                
+            }
+
+            void Even(int[] arr)
+            {
+                int n = 0;
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] % 2 == 0)
+                    {
+                        n++;
+                    }
+                }
+                Console.WriteLine($"\nКоличество четных чисел: {n}");
+            }
+
+
+            FillArray(array);
+            PrintArray(array);
+            Even(array);
+            
+        }
+    }
+}
